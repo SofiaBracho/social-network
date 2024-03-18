80b74bad0f7b744a4572cb0e70405a296b74a191
SofiaBracho <sofia_bracho1@hotmail.com>
2024-03-18 13:46:41 -0400

    Deploying to railway

--- project4/settings.py
+++ project4/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-8a70.up.railway.app', 
+    'https://web-production-8a70.up.railway.app/login',
+    'https://web-production-8a70.up.railway.app/register'
+]
 
 
 # Application definition
