4311510ee351cf890f938b0f2960976bbf7a847a
SofiaBracho <sofia_bracho1@hotmail.com>
2024-03-18 13:57:12 -0400

    Collecting static files

--- project4/settings.py
+++ project4/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+from pathlib import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -126,4 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+
 LOGIN_URL = '/login'
